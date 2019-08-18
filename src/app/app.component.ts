f29ccbf274889648e48403b241a5440b9f44dd6e
Laurentiu_20c <dobresculaurentiu20@gmail.com>
2019-08-18 10:31:27 +0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { COURSES } from '../db-data';
+import { Course } from './model/course';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  courses = COURSES;
+
+  title = COURSES[0].description;
+
+  price = 9.99;
+
+  startDate = new Date(2000, 0, 1);
+
+  course = COURSES[0];
+
+  onCourseSelected(course: Course) {
+    console.log('App component - click event bubbled ...', course);
+  }
+}
